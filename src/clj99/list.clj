2e765968c1e9721e98f75fd502e4305d3c43b7d8
Akira Ishino <akira.ishino@gmail.com>
2017-11-22 09:57:41 +0900

    P24 (*) Lotto: Draw N different random numbers from the set 1..M.

--- src/clj99/list.clj
+++ src/clj99/list.clj
@@ -358,3 +358,15 @@
     nil
     (conj (rnd-select lst (dec n))
           (element-at lst (inc (rand-int (my-count lst)))))))
+
+
+;; P24 (*) Lotto: Draw N different random numbers from the set 1..M.
+;; The selected numbers shall be returned in a list.
+;; Example:
+;; * (lotto-select 6 49)
+;; (23 1 17 33 21 37)
+;;
+;; Hint: Combine the solutions of problems P22 and P23.
+
+(defn lotto-select [n m]
+  (rnd-select (my-range 1 m) n))
